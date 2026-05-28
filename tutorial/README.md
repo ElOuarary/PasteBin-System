327bef6c98614df45aa055e58f51e2888e125544
El Ouarary Abdelaziz <abdelazizelouarary@gmail.com>
2026-05-28 19:34:42 +0100

    Stated the tutorial for flask webapp

---
+++ tutorial/README.md
@@ -0,0 +1,33 @@
+Basic blog application called Flaskr
+Users will be able to register, log in, create posts, and edit or delete their own posts
+You will be able to package and install the application on other computers
+
+Project Layout:
+SYSTEMDESIGNWITHFLASK/tutorial
+├── flaskr/
+│   ├── __init__.py
+│   ├── db.py
+│   ├── schema.sql
+│   ├── auth.py
+│   ├── blog.py
+│   ├── templates/
+│   │   ├── base.html
+│   │   ├── auth/
+│   │   │   ├── login.html
+│   │   │   └── register.html
+│   │   └── blog/
+│   │       ├── create.html
+│   │       ├── index.html
+│   │       └── update.html
+│   └── static/
+│       └── style.css
+├── tests/
+│   ├── conftest.py
+│   ├── data.sql
+│   ├── test_factory.py
+│   ├── test_db.py
+│   ├── test_auth.py
+│   └── test_blog.py
+├── .venv/
+├── pyproject.toml
+└── MANIFEST.in
---
+++ tutorial/flaskr/__init__.py
@@ -0,0 +1,34 @@
+import os
+from flask import Flask
+
+def create_app(test_config=None):
+    """
+    The application factory function, any configuration, registration and other setup
+    for the application needs will happen inside the function and it will returns he function
+    """
+    # Create and cofigure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY="dev",
+        DATABASE=os.path.join(app.instance_path, "flask.sqlite")
+    )
+    
+    if test_config is None:
+        # Load the instance config, if it exists, when not testing
+        app.config.from_pyfile("config.py", silent=True)
+    else:
+        # Load the test config if passed in 
+        app.config.from_mapping(test_config)
+        
+    # ensure the instance folder exists
+    os.makedirs(app.instance_path, exist_ok=True)
+    
+    from . import db
+    db.init_app(app)
+    
+    # a simple page that says hello
+    @app.route('/hello')
+    def hello():
+        return 'Hello, World!'
+
+    return app
