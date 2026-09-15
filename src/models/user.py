2ee0ab3f4dbdcc5f356d162d82e4c79bf87fa2c3
ElOuarary <abdlazizelouarary@gmail.com>
2026-09-15 09:42:07 +0100

    Swith to using default_facotry for the created_at field, since using default returns one value for the valuetimestamp for the first run and remains unchanged for the rest of the requests in the future, and cascade the user table with paste table since their relationhip is one to many

--- src/models/user.py
+++ src/models/user.py
@@ -15,4 +15,4 @@ class User(SQLModel, table=True):
     email: str
     created_at: datetime = Field(default_factory=lambda: datetime.now(UTC), index=True)
 
-    linked_pastes: list["Paste"] = Relationship(back_populates="linked_user")
+    linked_pastes: list["Paste"] = Relationship(back_populates="linked_user", cascade_delete=True)
