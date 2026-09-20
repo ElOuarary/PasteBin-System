e71ad060eca7c0f98316f517be5727c51720eca7
ElOuarary <abdlazizelouarary@gmail.com>
2026-09-20 21:25:30 +0100

    Added the dependecies for checking the auhentification of the user, by decoding the token and extracting the user id, now need, it serverless as the server need only the request to check for the user validaty and there is no lookup to the database to extract the user, but need to think for the design decision if I keep that way or add a lookup to the database for the user authentification especailly if the user is no longer active and revoked

--- main.py
+++ main.py
@@ -1,42 +1,19 @@
-from datetime import datetime, UTC, timedelta
-import uuid
-
-import jwt
-
 from fastapi import FastAPI, Depends, status, HTTPException
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-from jwt.exceptions import InvalidTokenError
 from pwdlib import PasswordHash
-from pydantic import BaseModel
+
 from sqlmodel import select
 
+from src.auth import Token, create_access_token
 from src.config.database import SessionDep
 from src.schemas.user import UserRegistryForm
 from src.models.user import User
 from routers import paste, pastes
 
-SECRET_KEY = "a1606a690dfc05a18f8165f41200d26ff23925ce78b61628bfa6bdf60f1e5a88"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 15
-
 password_hash = PasswordHash.recommended()
-
-class Token(BaseModel):
-    access_token: str
-    token_type: str
 
 oauth2_schema = OAuth2PasswordBearer("login")
 
-
-def create_access_token(username: str):
-    now = datetime.now(UTC)
-    payload = {
-        "sub": username,
-        "jti": str(uuid.uuid4()),
-        "iat": now,
-        "exp": now + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES),
-    }
-    return jwt.encode(payload=payload, key=SECRET_KEY, algorithm=ALGORITHM)
 
 app = FastAPI()
 app.include_router(paste.router)
@@ -67,4 +44,4 @@ def login(session: SessionDep, login_form: OAuth2PasswordRequestForm = Depends()
     if user is None or not password_hash.verify(login_form.password, user.hashed_password):
         # we can run seperate condition by first checking the existing of the username if it does not exists, we verify the hash to prevent time attacking
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Username or passowrd is invalid", headers={"WWW-Authenticate": "Bearer"})
-    return {"access_token": create_access_token(user.username), "token_type": "Bearer"}
+    return {"access_token": create_access_token(user.id), "token_type": "Bearer"}
